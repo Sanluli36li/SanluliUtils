25abac9d1befa40a27c44d6e7f1e8bf292602b0a
Leeroy Lee <Sanluli36li@outlook.com>
2025-05-19 11:25:51 +0800

    bugfix
    Fix error when keystone has no metadata

--- Modules/Social/ChatLink.lua
+++ Modules/Social/ChatLink.lua
@@ -111,7 +111,7 @@ local function HandleItemLink(itemLink)
     ]]
     if classID == Enum.ItemClass.Reagent and subclassID == Enum.ItemReagentSubclass.Keystone then
         local data = strsplittable(":", metaData)
-            if data[16] and data[18] then
+        if tonumber(data[16]) and tonumber(data[18]) then
             local mapName = C_ChallengeMode.GetMapUIInfo(tonumber(data[16]))
             if mapName then
                 displayItemName = CHALLENGE_MODE_KEYSTONE_HYPERLINK:format(mapName, tonumber(data[18]))
