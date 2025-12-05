1bbb9e19fb4e9009e3c84c6ae9ad583e9dbf2eb4
Leeroy Lee <Sanluli36li@outlook.com>
2025-12-06 03:07:57 +0800

    Update chat tab switch hook implementation
    Replaces hooksecurefunc with ChatFrame1EditBox:HookScript for handling OnTabPressed event. This change improves event handling specificity for the chat tab switch feature.

--- Modules/Social/ChatTabSwitch.lua
+++ Modules/Social/ChatTabSwitch.lua
@@ -60,7 +60,8 @@ end
 
 local lastChatType = 1
 
-hooksecurefunc("ChatEdit_OnTabPressed", function(editBox)
+-- hooksecurefunc("ChatEdit_OnTabPressed", function(editBox)
+ChatFrame1EditBox:HookScript("OnTabPressed", function(editBox)
     if Module:GetConfig(CONFIG_ENABLE) and editBox == ChatFrame1EditBox then
         if not (strsub(editBox:GetText(), 1, 1) == "/") then    -- "/"开头命令自动补全时不执行
             -- print("Current: ", editBox:GetAttribute("chatType"), CHAT_TYPE_ID[editBox:GetAttribute("chatType")])
