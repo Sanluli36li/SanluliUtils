a4e7b47159d2393fa6ba87d9af8c3cbd9a1a8489
Leeroy Lee <Sanluli36li@outlook.com>
2026-04-14 18:25:48 +0800

    fix: 尝试修复特定情况下钥匙链接解析错误的问题

--- Modules/Social/ChatLink.lua
+++ Modules/Social/ChatLink.lua
@@ -111,10 +111,13 @@ local function HandleItemLink(itemLink)
     ]]
     if classID == Enum.ItemClass.Reagent and subclassID == Enum.ItemReagentSubclass.Keystone then
         local data = strsplittable(":", metaData)
-        if tonumber(data[16]) and tonumber(data[18]) then
-            local mapName = C_ChallengeMode.GetMapUIInfo(tonumber(data[16]))
+        local mapID = tonumber(data[16])
+        local keystoneLevel = tonumber(data[18])
+
+        if mapID and keystoneLevel then
+            local mapName = C_ChallengeMode.GetMapUIInfo(mapID)
             if mapName then
-                displayItemName = CHALLENGE_MODE_KEYSTONE_HYPERLINK:format(mapName, tonumber(data[18]))
+                displayItemName = CHALLENGE_MODE_KEYSTONE_HYPERLINK:format(mapName, keystoneLevel)
             end
         end
     end
